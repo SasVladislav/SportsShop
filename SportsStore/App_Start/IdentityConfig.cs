615b69b2cb5bb35b95c019e9a05a1fab22b2fffb
Vlad Sas <vladyslav.sas@gmail.com>
2017-04-07 13:30:35 +0300

    Add Identity in project
    -add IdentityContext class
    -add IdentityInitializer class
    -add StoreUser class
    -add StoreRole class
    -add StoreUserManager class
    -add StoreRoleManager class
    -add identityConfig class in OWINSturtup

--- SportsStore/App_Start/IdentityConfig.cs
+++ SportsStore/App_Start/IdentityConfig.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
+using SportsStore.Infrastructure;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +12,16 @@ namespace SportsStore
 {
     public class IdentityConfig
     {
-        public void Configuration(IAppBuilder app) { }
+        public void Configuration(IAppBuilder app) {
+            app.CreatePerOwinContext<StoreIdentityDbContext>(
+                StoreIdentityDbContext.Create);
+            app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
+            app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+        }
     }
 }
---
+++ SportsStore/Infrastructure/StoreIdentityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SportsStore.Infrastructure
+{
+    public class StoreIdentityDbContext:IdentityDbContext<StoreUser>
+    {
+        public StoreIdentityDbContext():base("SportStoreIdentityDb")
+        {
+            Database.SetInitializer<StoreIdentityDbContext>(new
+                StoreIdentityDbInitializer());
+        }
+        public static StoreIdentityDbContext Create()
+        {
+            return new StoreIdentityDbContext();
+        }
+    }
+}
