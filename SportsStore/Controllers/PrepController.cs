4e2f81cc4067981825a0913da26ec451122b0a3f
Vlad Sas <vladyslav.sas@gmail.com>
2017-04-07 09:35:16 +0300

    Add project files.

---
+++ SportsStore/Controllers/PrepController.cs
@@ -0,0 +1,52 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class PrepController : Controller
+    {
+        IRepository repository;
+        public PrepController()
+        {
+            repository = new ProductRepository();
+        }
+        // GET: Prep
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            await repository.DeleteProductAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> SaveProduct(Product product)
+        {
+            await repository.SaveProductAsync(product);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Orders()
+        {
+            return View(repository.Orders);
+        }
+
+        public async Task<ActionResult> DeleteOrder(int order)
+        {
+            await repository.DeleteOrderAsync(order);
+            return RedirectToAction("Oders");
+        }
+
+        public async Task<ActionResult> SaveOrder(Order order)
+        {
+            await repository.SaveOrderAsync(order);
+            return RedirectToAction("Orders");
+        }
+            }
+}
